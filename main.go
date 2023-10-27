7cce70d4f9b92b3c36663e22ddb4120051c583e6
uni51 <unieye51@gmail.com>
2023-10-27 15:49:17 +0900

    169. クロージャとforloopを使ったGoroutineの注意点

--- main.go
+++ main.go
@@ -1,28 +1,39 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
+import "sync"
 
-func sayHello(wg *sync.WaitGroup) {
-	defer wg.Done()
-	fmt.Println("Hello")
-}
-
+/*
 func main() {
 	var wg sync.WaitGroup
 
-	wg.Add(1)
-
-	go sayHello(&wg)
+	say := "Hello"
 
 	wg.Add(1)
 
 	go func() {
 		defer wg.Done()
-		fmt.Println("Hello from anonymous function")
+		say = "Good Bye"
 	}()
+
+	wg.Wait()
+
+	fmt.Println(say) // Good Bye
+}
+*/
+
+func main() {
+	var wg sync.WaitGroup
+
+	tasks := []string{"A", "B", "C"}
+
+	for _, task := range tasks {
+		wg.Add(1)
+
+		go func(task string) {
+			defer wg.Done()
+			println(task) // C B A
+		}(task)
+	}
 
 	wg.Wait()
 }

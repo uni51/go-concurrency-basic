f9a4cd2964b5aed75040bb7a927baedf44292cc1
uni51 <unieye51@gmail.com>
2023-10-27 15:40:58 +0900

    168. Goroutine基礎

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func sayHello(wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println("Hello")
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+
+	go sayHello(&wg)
+
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		fmt.Println("Hello from anonymous function")
+	}()
+
+	wg.Wait()
+}
